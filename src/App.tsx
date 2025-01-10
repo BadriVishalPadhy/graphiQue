187ec372f8a6efd54fde8d95fc046be8e4f2710c
Bismay <bismaysarangi@gmail.com>
2025-01-10 12:51:39 +0530

    PaintZone component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Button } from "../src/components/ui/button";
+import PaintZone from "./components/ui/PaintZone";
+import "./App.css";
 
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <PaintZone />
     </div>
   );
 }
