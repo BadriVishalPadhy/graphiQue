73bfb28e787423dd713139deaf23f84d13166213
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-01-10 17:07:08 +0530

    basic-functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+<<<<<<< HEAD
 import CreativeEditorSDKComponent from "../src/components/CreativeEditorSDK";
+=======
+import { Button } from "../src/components/ui/button";
+import PaintZone from "./components/ui/PaintZone";
+import "./App.css";
+>>>>>>> 847e185bb8c821186bb11afcd65259ab03c3549f
 
 export default function Home() {
   return (
     <div>
+<<<<<<< HEAD
       <CreativeEditorSDKComponent />
       <div>{import.meta.env.VITE_license}</div>
+=======
+      <Button>Click me</Button>
+      <PaintZone />
+>>>>>>> 847e185bb8c821186bb11afcd65259ab03c3549f
     </div>
   );
 }
